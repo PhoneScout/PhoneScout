7262a974ebacd812cf34905803580d6aec77aa2d
Pocsai Gergő <90418238+gergopocsai@users.noreply.github.com>
2025-03-30 19:51:20 +0200

    Delete backend directory

--- backend/PhonesAPI3/Models/RegisterUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PhonesAPI3.Models
-{
-    public class RegisterUser
-    {
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
