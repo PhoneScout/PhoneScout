7d4ca4d479c5db083a84e6b53b6fa63cd235920a
Pocsai Gergő <pocsaig1@kkszki.hu>
2025-10-17 07:34:29 +0200

    react bugfix

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css'
+import './App.css';
+import Navbar from '../src/components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Halo</h1>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
