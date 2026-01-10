8a56b5a04d3b53cadeea353d8ba1687dea1cc4ec
Pocsai Gergő <pocsaig1@kkszki.hu>
2026-01-10 08:06:50 +0100

    Összehasonlítás backend
    Összehasonlítás oldalhoz szükséges Controller és DTO.

---
+++ backend/PhoneScout_GitHub/Controllers/comparePageController.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Mvc;
+using PhoneScout_GitHub.DTOs;
+using PhoneScout_GitHub.Models;
+
+namespace PhoneScout_GitHub.Controllers
+{
+    public class comparePageController : ControllerBase
+    {
+        private readonly PhoneContext _context;
+
+        public comparePageController(PhoneContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("comparePage/{id}")]
+
+        public IActionResult comparePage(int id)
+        {
+            var phone = _context.Phonedatas
+                .Where(p => p.PhoneId == id)
+            .Select(p => new comparePageDTO
+            {
+                phoneID = p.PhoneId,
+                phoneName = p.PhoneName,
+                phoneAntutu = p.PhoneAntutu,
+                phoneResolutionHeight = p.PhoneResolutionHeight,
+                phoneResolutionWidth = p.PhoneResolutionWidth,
+                screenSize = p.ScreenSize,
+                screenMaxBrightness = p.ScreenMaxBrightness,
+                screenSharpness = p.ScreenSharpness,
+                connectionMaxWifi = p.ConnectionMaxWifi,
+                connectionMaxBluetooth = p.ConnectionMaxBluetooth,
+                connectionMaxMobileNetwork = p.ConnectionMaxMobileNetwork,
+
+                batteryCapacity = p.BatteryCapacity,
+                batteryMaxChargingWired = p.BatteryMaxChargingWired,
+                batteryMaxChargingWireless = p.BatteryMaxChargingWireless,
+
+                phonePrice = p.PhonePrice,
+                phoneInStore = p.PhoneInStore,
+
+                cpuName = p.Cpu.CpuName,
+                cpuMaxClockSpeed = p.Cpu.CpuMaxClockSpeed,
+                cpuCoreNumber = p.Cpu.CpuCoreNumber,
+                cpuManufacturingTechnology = p.Cpu.CpuManufacturingTechnology,
+
+                ramSpeed = p.RamSpeed.RamSpeed1,
+                screenType = p.ScreenType.ScreenType1,
+                sensorsFingerprintPlace = p.SensorsFingerprintPlace.SensorsFingerprintPlace1,
+                sensorsFingerprintType = p.SensorsFingerprintType.SensorsFingerprintType1,
+                storageSpeed = p.StorageSpeed.StorageSpeed1,
+                waterproofType = p.WaterproofType.WaterproofType1,
+
+                ramStorage = p.Connphoneramstorages.Select(p => new ramStorageDTO
+                {
+                    ramAmount = p.Ramstorage.RamAmount,
+                    storageAmount = p.Ramstorage.StorageAmount
+                }).ToList()
+            });
+
+        
+            return Ok(phone);
+        }
+    }
+}
---
+++ backend/PhoneScout_GitHub/DTOs/comparePageDTO.cs
@@ -0,0 +1,42 @@
+using PhoneScout_GitHub.Models;
+
+namespace PhoneScout_GitHub.DTOs
+{
+    public class comparePageDTO
+    {
+
+        //Egyszerű adatok
+        public int? phoneID { get; set; }
+        public string? phoneName { get; set; }
+        public int? phoneAntutu { get; set; }
+        public int? phoneResolutionHeight { get; set; }
+        public int? phoneResolutionWidth { get; set; }
+        public string? screenType { get; set; }
+        public decimal? screenSize { get; set; }
+        public int? screenRefreshRate { get; set; }
+        public int? screenMaxBrightness { get; set; }
+        public int? screenSharpness { get; set; }
+        public int? connectionMaxWifi { get; set; }
+        public decimal? connectionMaxBluetooth { get; set; }
+        public int? connectionMaxMobileNetwork { get; set; }
+        public int? batteryCapacity { get; set; }
+        public int? batteryMaxChargingWired { get; set; }
+        public int? batteryMaxChargingWireless { get; set; }
+        public int? phonePrice { get; set; }
+        public string? phoneInStore { get; set; }
+
+        //Adatok kapcsolótáblákból
+        public string? cpuName { get; set; }
+        public int? cpuMaxClockSpeed { get; set; }
+        public int? cpuCoreNumber { get; set; }
+        public int? cpuManufacturingTechnology { get; set; }
+        public string? ramSpeed { get; set; }
+        public string? sensorsFingerprintPlace { get; set; }
+        public string? sensorsFingerprintType { get; set; }
+        public string? storageSpeed { get; set; }
+        public string? waterproofType { get; set; }
+
+        //Összetett adatok
+        public List<ramStorageDTO> ramStorage { get; set; }
+    }
+}
